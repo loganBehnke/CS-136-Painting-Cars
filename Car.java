826c20e193e286828816689948ad19214dc3fad5
loganBehnke <lmb548@nau.edu>
2017-02-14 21:17:55 -0700

    Final code

--- Car.java
+++ Car.java
@@ -4,29 +4,35 @@ import java.awt.geom.Line2D;
 import java.awt.Rectangle;
 import java.awt.geom.Point2D;
 import java.awt.Color;
+import javax.swing.JComponent;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
 
 
-public class Car{
+public class Car extends JComponent{
   int x;
   int y;
-  String color;
+  Color carColor;
   int lengthofcar;
   int frontTireSize;
   int rearTireSize;
 
-  public Car(int x, int y, String color, int length, int frontTireSize, int rearTireSize){
+  public Car(int x, int y, Color carColor, int length, int frontTireSize, int rearTireSize){
     this.x = x;
     this.y = y;
-    this.color = color;
+    this.carColor = carColor;
     this.lengthofcar = length;
     this.frontTireSize = frontTireSize;
     this.rearTireSize = rearTireSize;
 
   }
 
-  public void draw(Graphics2D g2){
+  @Override
+  protected void paintComponent(Graphics g){
+    Graphics2D g2 = (Graphics2D) g;
+    g2.translate(this.x, this.y);
     Rectangle carbody = new Rectangle(0, 10, this.lengthofcar, 10);
-    g2.setColor(Color.getColor(color));
+    g2.setColor(this.carColor);
     g2.fill(carbody);
     Ellipse2D.Double frontTire = new Ellipse2D.Double((this.lengthofcar-20), 20, this.frontTireSize, this.frontTireSize);
     g2.setColor(Color.BLACK);
@@ -39,16 +45,16 @@ public class Car{
     Point2D.Double r1 = new Point2D.Double((this.lengthofcar-10), 10);
     //front of the roof
     Point2D.Double r2 = new Point2D.Double((this.lengthofcar-20), 0);
-    //rear of teh roof
+    //rear of the roof
     Point2D.Double r3 = new Point2D.Double(10,10);
     //Bottom of the rear windowshield
     Point2D.Double r4 = new Point2D.Double(20, 0);
 
     Line2D.Double frontWindoshield = new Line2D.Double(r1, r2);
-    Line2D.Double rooftop = new Line2D.Double(r2, r3);
+    Line2D.Double rooftop = new Line2D.Double(r2, r4);
     Line2D.Double rearWindoshield = new Line2D.Double(r3, r4);
 
-    g2.setColor(Color.getColor(color));
+    g2.setColor(this.carColor);
     g2.draw(frontWindoshield);
     g2.draw(rooftop);
     g2.draw(rearWindoshield);
--- CustomCarComponent.java
+++ CustomCarComponent.java
@@ -11,6 +11,19 @@ public class CustomCarComponent{
     frame.setBackground(Color.GRAY);
     frame.setVisible(true);
 
-    
+
+    PaintBucket bucket = new PaintBucket();
+    bucket.addColor(255, 0, 0, 1);
+    Car car1 = new Car(10, 10, bucket.getMixedColor(), 60, 10, 10);
+    frame.add(car1);
+    frame.setVisible(true);
+    bucket.addColor(0, 0, 255, 2);
+    Car car2 = new Car(10, 50, bucket.getMixedColor(), 100, 10, 15);
+    frame.add(car2);
+    frame.setVisible(true);
+    bucket.addColor(0, 0, 255, 4);
+    Car car3 = new Car(10, 100, bucket.getMixedColor(), 45, 5, 6);
+    frame.add(car3);
+    frame.setVisible(true);
   }
 }
