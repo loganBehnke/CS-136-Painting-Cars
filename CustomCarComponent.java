9ab15774ca6c362e3fc35e11403c24ae6330f8a8
loganBehnke <lmb548@nau.edu>
2017-02-14 19:57:57 -0700

    Made the  CustomCarComponent class

---
+++ CustomCarComponent.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+import java.awt.Color;
+
+public class CustomCarComponent{
+  public static void main(String[] args){
+    //sets up the JFrame
+    JFrame frame = new JFrame();
+    frame.setSize(220, 520);
+    frame.setTitle("Custom Cars!");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setBackground(Color.GRAY);
+    frame.setVisible(true);
+
+    
+  }
+}
